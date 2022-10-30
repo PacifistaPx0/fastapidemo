4fb4d0705fe417f95c16bacd1718c5f26eb93f45
PacifistaPx0 <richieadigwe@gmail.com>
2022-10-30 15:04:36 +0100

    made a correction to main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"SlackUsername":"pacifistapx0", "backend":True, "age":25, "bio":"The most important step a man can take is always the next one"}
+    return {"slackUsername":"pacifistapx0", "backend":True, "age":25, "bio":"The most important step a man can take is always the next one"}
