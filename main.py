add85930afb76dbcaad681306ad384fbc1ec1793
PacifistaPx0 <richieadigwe@gmail.com>
2022-11-05 01:16:15 +0100

    created a post route

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-from fastapi import FastAPI
+from enum import Enum 
+from fastapi import Body, FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class operation_type(Enum):
+    multiplication= 1
+    addition= 2
+    subtraction = 3
+
+class Post(BaseModel):
+    operation_type: str
+    x: int
+    y: int
+
+operation_list = ["multiplication", "addition", "subtraction"]
 
 @app.get("/")
 def root():
     return {"slackUsername":"pacifistapx0", "backend":True, "age":25, "bio":"The most important step a man can take is always the next one"}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post:Post):
+    print(post)
+    value = 0
+    if post.operation_type.lower() not in operation_list:
+        return Response(status_code=status.HTTP_406_NOT_ACCEPTABLE)
+    try:
+        if post.operation_type.lower() == operation_type.multiplication.name:
+            value = post.x * post.y
+            return {"slackUsername": "pacifistapx0", "result": value, "operation_type":operation_type.multiplication.value}
+        elif post.operation_type.lower() == operation_type.addition.name:
+            value = post.x + post.y
+            return {"slackUsername": "pacifistapx0", "result": value, "operation_type":operation_type.addition.value}
+        else:
+            post.operation_type.lower() == operation_type.subtraction.name
+            value = post.x - post.y
+            return {"slackUsername": "pacifistapx0", "result": value, "operation_type":operation_type.subtraction.value}
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, 
+        detail="Operation not available")
+    
